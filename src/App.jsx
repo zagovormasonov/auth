e29ffad2459fcde0e5aa94ffe3fcac91a5cdd6a2
noName <samyrize77777@gmail.com>
2025-04-01 19:22:06 +0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
-import Dashboard from "./Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   return (
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "./supabaseClient";
+import { supabase } from "../supabaseClient";
 import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
