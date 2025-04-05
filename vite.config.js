34066b33f95f8f0e00598953f5070d8f2547381f
noName <samyrize77777@gmail.com>
2025-04-06 01:20:46 +0400

    modify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Должно быть именно "/" (не "./" и не "/public/")
+  plugins: [tailwindcss(),react()],
+  base: '/',
 });
